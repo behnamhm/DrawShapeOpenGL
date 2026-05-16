0b1fd20f015eaf7c84bd561ab536b11f4feb150b
behna <behnam1171@gmail.com>
2026-05-16 13:58:44 -0700

    Created the window

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,32 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 int main()
 {
+    if (!glfwInit())
+    {
+        return -1;
+    }
+    
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "GameEngineApp", nullptr, nullptr);
+
+    if (window == nullptr)
+    {
+        std::cout << "Error Creating window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    //glfwSetWindowPos(window, 2000, 150);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+
+
     return 0;
 }
